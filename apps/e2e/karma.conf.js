0bc103136127f68307b9554eececea7c6740eaf1
John White <750350+johnhwhite@users.noreply.github.com>
2023-06-20 23:52:25 -0400

    ci: test configuration update

--- apps/e2e/karma.conf.js
+++ apps/e2e/karma.conf.js
@@ -1,45 +1,6 @@
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('karma-chrome-launcher'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-    ],
-    client: {
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true, // removes the duplicated traces
-    },
-    coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/e2e'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'json-summary' },
-        { type: 'text-summary' },
-        { type: 'lcov', subdir: 'lcov-report' },
-      ],
-      check: {
-        global: {
-          statements: 100,
-          branches: 100,
-          functions: 100,
-          lines: 100,
-        },
-      },
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: true,
-    restartOnFileChange: true,
-  });
+  require('../../karma.conf')(
+    config,
+    require('path').join(__dirname, '../../coverage/e2e')
+  );
 };
