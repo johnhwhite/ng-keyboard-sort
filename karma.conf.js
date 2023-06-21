f4820178c3e6abffec6dc1af10a845d1cad5956f
John White <750350+johnhwhite@users.noreply.github.com>
2023-06-20 23:57:24 -0400

    ci: test configuration update (#69)

--- karma.conf.js
+++ karma.conf.js
@@ -1,43 +1,52 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
-const { join } = require('path');
-const { constants } = require('karma');
-
-module.exports = () => {
-  return {
+module.exports = function (config, coverageDir) {
+  config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
+      require('karma-chrome-launcher'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: join(__dirname, './coverage'),
+      dir: coverageDir,
       subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+      reporters: [
+        { type: 'html' },
+        { type: 'json-summary' },
+        { type: 'text-summary' },
+        { type: 'lcov', subdir: 'lcov-report' },
+      ],
+      check: {
+        global: {
+          statements: 100,
+          branches: 100,
+          functions: 100,
+          lines: 100,
+        },
+      },
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
-    logLevel: constants.LOG_INFO,
+    logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--disable-translate', '--disable-extensions', '--no-sandbox'],
+      },
+    },
     singleRun: true,
-  };
+    concurrency: Infinity,
+    restartOnFileChange: true,
+  });
 };
