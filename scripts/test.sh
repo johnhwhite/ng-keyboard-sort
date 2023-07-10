dba7f460762333325da01bf853ca5e918b7d0eb9
John White <750350+johnhwhite@users.noreply.github.com>
2023-07-10 16:07:08 -0400

    Wrap tests in script

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,5 @@
 
 set -eo pipefail
 
-echo '# ng test ng-keyboard-sort'
-npx ng test ng-keyboard-sort -c ci | cat
-echo '# ng test e2e'
-npx ng test e2e -c ci | cat
+cd "$(dirname "$0")/.."
+node scripts/test.js
