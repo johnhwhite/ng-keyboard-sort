00771b2f85280cb567091db99aeaa533114e34fa
John White <750350+johnhwhite@users.noreply.github.com>
2024-09-27 14:44:01 -0400

    ci: update pages workflow to set baseHref

--- apps/e2e/src/main.ts
+++ apps/e2e/src/main.ts
@@ -1,9 +1,8 @@
-import { importProvidersFrom } from '@angular/core';
-import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app-routes';
-import { provideRouter } from '@angular/router';
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule), provideRouter(routes)],
+  providers: [provideRouter(routes)],
 }).catch((err) => console.error(err));
