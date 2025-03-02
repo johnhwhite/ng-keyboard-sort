de06cfe1ee2c33bae6b8a53a3709bca5d2e975a1
John White <750350+johnhwhite@users.noreply.github.com>
2025-03-02 09:52:15 -0500

    feat(ng-keyboard-sort): option to override keys

--- libs/ng-keyboard-sort/src/index.ts
+++ libs/ng-keyboard-sort/src/index.ts
@@ -2,5 +2,6 @@ export { KeyboardSortHandleDirective } from './lib/keyboard-sort-handle.directiv
 export { KeyboardSortItemDirective } from './lib/keyboard-sort-item.directive';
 export { KeyboardSortItemIfActiveDirective } from './lib/keyboard-sort-item-if-active.directive';
 export { KeyboardSortItemIfFocusedDirective } from './lib/keyboard-sort-item-if-focused.directive';
+export { KeyboardSortKeysInterface } from './lib/keyboard-sort-keys.interface';
 export { KeyboardSortListDirective } from './lib/keyboard-sort-list.directive';
 export { KeyboardSortModule } from './lib/keyboard-sort.module';
