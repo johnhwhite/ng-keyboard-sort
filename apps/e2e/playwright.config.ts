54f36f11f67add3777678f5e0a23f3a28ff3a0e9
John White <750350+johnhwhite@users.noreply.github.com>
2025-03-30 00:48:48 -0400

    build: switch to playwright-ng-schematics builder (#154)

--- apps/e2e/playwright.config.ts
+++ apps/e2e/playwright.config.ts
@@ -1,14 +1,35 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
-const config = defineConfig({
-  outputDir: '../../dist/playwright',
+export default defineConfig({
   testDir: './tests',
-  webServer: {
-    command: 'npx ng s e2e --port 4204',
-    port: 4204,
-    timeout: 120 * 1000,
-    cwd: '../..',
+  reporter: [
+    ['html', { outputFile: 'test-results/report.html' }],
+    ['json', { outputFile: 'test-results/results.json' }],
+    ['junit', { outputFile: 'test-results/results.xml' }],
+  ],
+  use: {
+    baseURL: process.env['PLAYWRIGHT_TEST_BASE_URL'] || 'http://localhost:4200',
   },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
+  ],
 });
-
-export default config;
