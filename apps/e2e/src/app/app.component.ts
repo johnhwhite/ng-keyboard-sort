6728f998d1728794c51c97986714663dbff5885f
John White <750350+johnhwhite@users.noreply.github.com>
2025-06-01 22:15:53 -0400

    build: add repo link to e2e pages

--- apps/e2e/src/app/app.component.ts
+++ apps/e2e/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterLink, RouterOutlet, ROUTES } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrl: './app.component.css',
   imports: [RouterLink, RouterOutlet],
 })
 export class AppComponent {
