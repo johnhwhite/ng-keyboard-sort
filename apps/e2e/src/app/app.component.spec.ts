00f2cbb2bff843fe1938cf22c42240e977ea4908
John White <750350+johnhwhite@users.noreply.github.com>
2025-06-01 22:19:26 -0400

    build: add repo link to e2e pages (#161)

--- apps/e2e/src/app/app.component.spec.ts
+++ apps/e2e/src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { provideRouter } from '@angular/router';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, AppComponent],
-    }).compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideRouter([])],
+    });
   });
 
   it('should create the app', () => {
