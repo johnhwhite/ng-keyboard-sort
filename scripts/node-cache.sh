7992b9e6185c0a4eaf7c709fed95dd914d35507c
John White <750350+johnhwhite@users.noreply.github.com>
2025-09-10 01:06:31 -0400

    feat(ng-keyboard-sort): add Angular 20.2 support and use trusted publisher (#168)

--- scripts/node-cache.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# This script generates a SHA hash of package-lock.json dependencies field.
-# It is used to invalidate the node_modules cache when dependencies change.
-echo -n node-cache=
-jq -r .dependencies package-lock.json | shasum -a 256 | awk '{print $1}'
